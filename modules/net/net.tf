55a7a1336fd80658f744d1cac33b3772ecc26946
Kenneth Massada <kmassada@gmail.com>
2019-02-25 03:23:56 +0000

    abstract some of the important values of network to a file.

--- modules/net/net.tf
+++ modules/net/net.tf
@@ -1,12 +1,12 @@
 resource "google_compute_network" "neuron_tf" {
-  name                    = "neuron-tf"
+  name                    = "${var.network}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "neuron_subnet_tf" {
-  name          = "neuron-subnet-tf"
+  name          = "${var.subnetwork}"
   network       = "${google_compute_network.neuron_tf.name}"
-  ip_cidr_range = "10.128.0.0/16"
+  ip_cidr_range = "${var.subnetwork_ip_cidr_range}"
 
   secondary_ip_range {
     range_name    = "containerrange1"
---
+++ modules/net/variables.tf
@@ -0,0 +1,11 @@
+variable "network" {
+  default = "neuron-tf"
+}
+
+variable "subnetwork" {
+  default = "neuron-subnet-tf"
+}
+
+variable "subnetwork_ip_cidr_range" {
+  default = "10.128.0.0/16"
+}
