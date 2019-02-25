622906e1277acb0506e7b51962f9b10a472f49f9
Kenneth Massada <kmassada@gmail.com>
2019-02-25 03:45:28 +0000

    rename the policy binding to avoid clash

--- plan/gke-pv-cluster/cluster-sa.tf
+++ plan/gke-pv-cluster/cluster-sa.tf
@@ -3,19 +3,19 @@ resource "google_service_account" "gke_node_sa" {
   display_name = "GKE Node Service Account"
 }
 
-resource "google_project_iam_binding" "metricWriter" {
+resource "google_project_iam_binding" "node_metricWriter" {
   role               = "roles/monitoring.metricWriter"
 
   members = ["serviceAccount:${google_service_account.gke_node_sa.email}"]
 }
 
-resource "google_project_iam_binding" "viewer" {
+resource "google_project_iam_binding" "node_monitoring_viewer" {
   role               = "roles/monitoring.viewer"
 
   members = ["serviceAccount:${google_service_account.gke_node_sa.email}"]
 }
 
-resource "google_project_iam_binding" "logWriter" {
+resource "google_project_iam_binding" "node_logWriter" {
   role               = "roles/logging.logWriter"
 
   members = ["serviceAccount:${google_service_account.gke_node_sa.email}"]
