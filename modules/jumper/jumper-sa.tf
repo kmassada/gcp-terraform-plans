6bc7eedd79583ee020c013192df9331f33cd9919
Kenneth Massada <kmassada@gmail.com>
2019-06-25 10:48:28 -0700

    members trigger tf changes

--- modules/jumper/jumper-sa.tf
+++ modules/jumper/jumper-sa.tf
@@ -8,6 +8,9 @@ resource "google_project_iam_binding" "jumper_metricWriter" {
   role = "roles/monitoring.metricWriter"
   project = "${var.project_id}"
   members = ["serviceAccount:${google_service_account.jumper_sa.email}"]  
+  lifecycle {
+    ignore_changes =["members"]
+  }
   depends_on   = [
     "google_service_account.jumper_sa"
   ]
@@ -17,6 +20,9 @@ resource "google_project_iam_binding" "jumper_monitoring_viewer" {
   role = "roles/monitoring.viewer"
   project = "${var.project_id}"
   members = ["serviceAccount:${google_service_account.jumper_sa.email}"]
+  lifecycle {
+    ignore_changes =["members"]
+  }
   depends_on   = [
     "google_service_account.jumper_sa"
   ]
@@ -26,6 +32,9 @@ resource "google_project_iam_binding" "jumper_logWriter" {
   role = "roles/logging.logWriter"
   project = "${var.project_id}"
   members = ["serviceAccount:${google_service_account.jumper_sa.email}"]
+  lifecycle {
+    ignore_changes =["members"]
+  }
   depends_on   = [
     "google_service_account.jumper_sa"
   ]
@@ -35,6 +44,9 @@ resource "google_project_iam_binding" "jumper_container_developer" {
   role = "roles/container.developer"
   project = "${var.project_id}"
   members = ["serviceAccount:${google_service_account.jumper_sa.email}"]
+  lifecycle {
+    ignore_changes =["members"]
+  }
   depends_on   = [
     "google_service_account.jumper_sa"
   ]
