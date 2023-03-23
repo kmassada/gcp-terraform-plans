435d39337022eb69c4c325b44e649a32925643cd
Kenneth Massada <kmassada@gmail.com>
2023-03-23 13:47:04 -0700

    declare folder

--- variables.tf
+++ variables.tf
@@ -15,6 +15,10 @@ variable "org_id" {
   description = "The ID of the Google Cloud Organization."
 }
 
+variable "folder_id" {
+  description = "The ID of the Google Cloud Folder."
+}
+
 variable "billing_account" {
   description = "The ID of the associated billing account (optional)."
 }
