bf4cbfaef20bac01410e6df4a1a6c5bf33925225
Kenneth Massada <kmassada@gmail.com>
2023-03-23 13:44:56 -0700

    pause cluster creation for now

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "cluster_name" {
-  value = "${module.gke_pv_cluster.cluster_name}"
-}
+# output "cluster_name" {
+#   value = "${module.gke_pv_cluster.cluster_name}"
+# }
 
-output "cluster_region" {
-  value = "${module.gke_pv_cluster.cluster_region}"
-}
+# output "cluster_region" {
+#   value = "${module.gke_pv_cluster.cluster_region}"
+# }
 
 output "project_id" {
   value = "${module.proj.project_id}"
