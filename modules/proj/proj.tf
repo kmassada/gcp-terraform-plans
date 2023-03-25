4223218a1f1bcd7fd0fd24423ccc4bce07563d5b
Kenneth Massada <kmassada@gmail.com>
2023-03-25 15:34:57 -0700

    proj id constraints

--- modules/proj/proj.tf
+++ modules/proj/proj.tf
@@ -7,11 +7,11 @@ terraform {
 }
 
 resource "random_id" "project_name" {
-  byte_length = 8
+  byte_length = 4
 }
 
 resource "google_project" "tf_project" {
-  name            = "Terraform Project ${random_id.project_name.hex}"
+  name            = "TF Proj ${random_id.project_name.hex}"
   project_id      = "tf-btstrp-proj-${random_id.project_name.hex}"
   folder_id       = var.folder_id
   billing_account = var.billing_account
