cc34229a0929cf6cf0a1c7e212c1066614ecff27
Kenneth Massada <kmassada@gmail.com>
2023-03-27 04:31:04 -0700

    project_services

---
+++ modules/project-services/variables.tf
@@ -0,0 +1,17 @@
+variable "services" {
+  description = "Service APIs to enable."
+  type        = list(string)
+  default     = []
+}
+
+variable "service_config" {
+  description = "Configure service API activation."
+  type = object({
+    disable_on_destroy         = bool
+    disable_dependent_services = bool
+  })
+  default = {
+    disable_on_destroy         = false
+    disable_dependent_services = false
+  }
+}
