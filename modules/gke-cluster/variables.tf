6a59aaf1d233eb86b18fcdf9d501cada5034e547
Kenneth Massada <kmassada@gmail.com>
2023-03-27 05:31:37 -0700

    add gke-cluster modules

---
+++ modules/gke-cluster/variables.tf
@@ -0,0 +1,27 @@
+variable "network" {
+    description =  "Terraform Generated network"
+}
+
+variable "subnet" {
+    description =  "Terraform Generated subnet"
+}
+
+variable "subnet_ip_cidr_range" {
+    description =  "Terraform Generated subnet's cidr range"
+}
+
+variable "project_id" {
+    description =  "Terraform Generated Project ID"    
+}
+
+variable "region" {
+    description =  "Terraform Set Region/Zone"    
+}
+
+variable "min_master_version" {
+    description =  "Terraform Set Min Master Version"    
+}
+
+variable "service_account_email" {
+    description =  "Email of the GKE Node Service Account"
+}
