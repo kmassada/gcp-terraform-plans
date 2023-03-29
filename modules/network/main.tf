3cbd4522e2d819602469d46a553d50af75f21865
Kenneth Massada <kmassada@gmail.com>
2023-03-29 03:29:53 -0700

    auto create_subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_network" "network" {
   name                    = var.network_name
   project                 = var.project_id
-  auto_create_subnetworks = false
+  auto_create_subnetworks = var.auto_create_subnetworks
   description 		        = "Network created by Terraform"
 }
 
