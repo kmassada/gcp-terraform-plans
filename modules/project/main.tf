f81781b904bfb545545a0703b130fe5474293142
Kenneth Massada <kmassada@gmail.com>
2023-04-01 20:05:06 -0700

    project_name_prefix and id_prefix

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -11,8 +11,8 @@ resource "random_id" "project_name" {
 }
 
 resource "google_project" "main" {
-  name            = "TF Proj ${random_id.project_name.hex}"
-  project_id      = "tf-btstrp-proj-${random_id.project_name.hex}"
+  name            = "${var.project_name_prefix} ${random_id.project_name.hex}"
+  project_id      = "${var.project_id_prefix}-${random_id.project_name.hex}"
   folder_id       = var.folder_id
   billing_account = var.billing_account
 }
--- modules/project/variables.tf
+++ modules/project/variables.tf
@@ -3,9 +3,19 @@ variable "org_id" {
 }
 
 variable "folder_id" {
-  description = "The ID of the Google Cloud Project."
+  description = "The ID of the Google Cloud Folder."
 }
 
 variable "billing_account" {
   description = "The ID of the associated billing account (optional)."
+}
+
+variable "project_name_prefix" {
+  description = "The Prefix of the Project Name."
+  default = "TF Proj"
+}
+
+variable "project_id_prefix" {
+  description = "The Prefix of the Project ID."
+  default = "tf-btstrp-proj"
 }
