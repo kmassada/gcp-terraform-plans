b60088bdc337beaf2ae42faa3ffc40f8e41ece43
Kenneth Massada <kmassada@gmail.com>
2023-04-03 13:23:49 -0700

    new backend module

---
+++ modules/tf-backend/main.tf
@@ -0,0 +1,29 @@
+resource "google_storage_bucket" "admin" {
+  name          = var.bucket_name
+  location      = "US"
+  force_destroy = true
+  project       = var.bucket_project
+  depends_on = [
+    module.project
+  ]
+}
+resource "terraform_data" "outputs" {
+  provisioner "local-exec" {
+    command = "mkdir -p ./output/"
+  }
+}
+
+resource "local_file" "backend_tf" {
+  filename = "./output/backend_tf"
+  content  = <<-EOT
+    terraform {
+        backend "gcs" {
+            bucket  = "${var.bucket_name}"
+            prefix  = "terraform/state"
+        }
+    }
+  EOT
+  depends_on = [
+    terraform_data.outputs
+  ]
+}
---
+++ modules/tf-backend/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  description = "The Name of the bucket being used for TF Backend"
+}
+
+variable "bucket_project" {
+  description = "The Project of the bucket being used for TF Backend"
+}
