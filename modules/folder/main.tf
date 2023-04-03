70b2bc5db72b4dfb43bea016bd0d30fecf50be18
Kenneth Massada <kmassada@gmail.com>
2023-04-03 12:41:52 -0700

    "organizations/${var.org_id}" ?

--- modules/folder/main.tf
+++ modules/folder/main.tf
@@ -1,4 +1,4 @@
 resource "google_folder" "main" {
   display_name = var.folder_name
-  parent       = var.enable_org_id ? var.org_id : var.folder_id
+  parent       = var.enable_org_id ? "organizations/${var.org_id}" : "folders/${var.folder_id}"
 }
