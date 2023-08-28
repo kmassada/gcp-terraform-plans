0c453b3fb5d8403bc399e2ecd6fcad5069015e37
Nadezhda Kabanova <nadinenadyuchka@gmail.com>
2023-08-28 14:33:11 -0400

    added draft readme

--- gke-clusters.tf
+++ gke-clusters.tf
@@ -1,39 +1,3 @@
-locals {
-  gke_cluster_config = {
-    gke-cluster-1 = {
-      "podrange"                = "10.8.0.0/14"
-      "servicerange"            = "10.12.0.0/20"
-      "enable_private_endpoint" = false
-      "create_custom_range"     = true
-      "network_name"            = module.network["tf-net"].network_name
-      "subnet_name"             = module.network["tf-net"].subnet_name["tf-subnet"]
-      "pod_range_name"          = var.tf_subnet_config.tf-subnet.podrange
-      "service_range_name"      = var.tf_subnet_config.tf-subnet.servicerange
-
-    }
-    gke-cluster-dpv2-pv-endpt = {
-      "location"                   = "us-west3-a"
-      "master_authorized_networks" = "10.0.0.0/8"
-
-    }
-    gke-cluster-dpv2-pub-int-endpt = {
-      "location"                = "us-west3-b"
-      "master_ipv4_cidr_block"  = "172.16.0.32/28"
-      "enable_private_endpoint" = false
-
-    }
-    gke-cluster-dpv2-pub-ext-endpt = {
-      "location"                = "us-west3-c"
-      "master_ipv4_cidr_block"  = ""
-      "enable_private_endpoint" = false
-
-    }
-  }
-
-
-}
-
-
 
 module "gke_cluster" {
   for_each                    = local.gke_cluster_config
@@ -53,7 +17,6 @@ module "gke_cluster" {
   master_global_access_config = try(each.value.master_global_access_config, true)
   create_custom_range         = try(each.value.create_custom_range, false)
   master_authorized_networks  = try(each.value.master_authorized_networks, "")
-
 
 }
 
--- variables.tf
+++ variables.tf
@@ -25,11 +25,6 @@ variable "api_services" {
   "container.googleapis.com"]
 }
 
-variable "admin_sa" {
-  description = "Admin ServiceAccount name to manage other Terraform resources."
-  default     = ""
-}
-
 
 variable "authorized_networks" {
   description = "Authorized networks CIDRs, used in firewall rules"
@@ -74,4 +69,39 @@ variable "service_accounts" {
     ]
 
   }
+}
+
+locals {
+  gke_cluster_config = {
+    gke-cluster-1 = {
+      "podrange"                = "10.8.0.0/14"
+      "servicerange"            = "10.12.0.0/20"
+      "enable_private_endpoint" = false
+      "create_custom_range"     = true
+      "network_name"            = module.network["tf-net"].network_name
+      "subnet_name"             = module.network["tf-net"].subnet_name["tf-subnet"]
+      "pod_range_name"          = var.tf_subnet_config.tf-subnet.podrange
+      "service_range_name"      = var.tf_subnet_config.tf-subnet.servicerange
+
+    }
+    gke-cluster-dpv2-pv-endpt = {
+      "location"                   = "us-west3-a"
+      "master_authorized_networks" = "10.0.0.0/8"
+
+    }
+    gke-cluster-dpv2-pub-int-endpt = {
+      "location"                = "us-west3-b"
+      "master_ipv4_cidr_block"  = "172.16.0.32/28"
+      "enable_private_endpoint" = false
+
+    }
+    gke-cluster-dpv2-pub-ext-endpt = {
+      "location"                = "us-west3-c"
+      "master_ipv4_cidr_block"  = ""
+      "enable_private_endpoint" = false
+
+    }
+  }
+
+
 }
